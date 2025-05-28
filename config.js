ccf92f64c3139dcf8172302b4fd6f78c748bd237
Takeo312 <thanthaison0@gmail.com>
2025-05-28 22:47:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,23 @@
 const CONFIG = {
-    titleWeb: "Gửi crush của tớ",
-    introTitle: 'Crush yêu dấu',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Gửi các anh chị đáng iu cụa em",
+    introTitle: 'Này người đẹp ơi',
+    introDesc: `Tháng năm vẫn lạ thường
+Mà sao ta cứ chung một đường
+Lạc vào HGC ngát xanh
+Đằng sau kỷ niệm đây là tình.
+
+Giờ anh chị lại rẽ lối
+Bước vào trời rộng lớn lao
+Chân mây gọi những ước mơ
+HGC vẫn mãi đợi chờ.
+
+Dù mai này xa bao la
+HGC vẫn là nhà.
+Một phần ở lại trong tim
+Để rồi ngày nào trở về, tìm.`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Thoát web là chấp nhận yêu tớ rồi nhé!!!',
+    desc: 'Thoát web là chấp nhận đi show rồi nhé!!!',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
