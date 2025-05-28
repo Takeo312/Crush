cf24fab4dca23ae559b806360bcdc18845975ec9
Takeo312 <thanthaison0@gmail.com>
2025-05-28 23:00:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,30 +1,17 @@
 const CONFIG = {
     titleWeb: "Gửi các anh chị đáng iu cụa em",
     introTitle: 'Này người đẹp ơi',
-    introDesc: `Tháng năm vẫn lạ thường
-Mà sao ta cứ chung một đường
-Lạc vào HGC ngát xanh
-Đằng sau kỷ niệm đây là tình.
-
-Giờ anh chị lại rẽ lối
-Bước vào trời rộng lớn lao
-Chân mây gọi những ước mơ
-HGC vẫn mãi đợi chờ.
-
-Dù mai này xa bao la
-HGC vẫn là nhà.
-Một phần ở lại trong tim
-Để rồi ngày nào trở về, tìm.`,
+    introDesc: `Sắp đến show rồi đó // Có đi show không hả!!!`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng anh chị đã đồng ý ghé show 🥰',
+    title: 'Anh chị mún đi show khum nè 🥰',
     desc: 'Thoát web là chấp nhận đi show rồi nhé!!!',
     btnYes: 'Thích lắm <33',
-    btnNo: 'Không có chuyện là không đi nha :3',
+    btnNo: 'Không đi đâu',
     question: 'Có muốn đi show không ạ',
     btnReply: 'Có hoặc không nha',
     reply: 'Chắc chắn là đi rồi!',
     mess: 'Em biết mà 🥰. Yêu anh chị nhiều nhiều 😘😘',
     messDesc: 'Tối sau, em qua đón đi show nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'link HGC' //link mess của các bạn. VD: /dinhkaito
+    messLink: 'https://www.facebook.com/hnueguitarclub' //link mess của các bạn. VD: /dinhkaito
 }
